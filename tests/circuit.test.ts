73616de0c31449e5f3186df60755db419849e673
ashWhiteHat <phantomofrotten@gmail.com>
2025-05-05 14:06:04 +0900

    circuit test

--- tests/circuit.test.ts
+++ tests/circuit.test.ts
@@ -1,6 +1,11 @@
+const wasm = require("circom_tester").wasm;
+
 describe("Circuit", () => {
-  it("should be a test", () => {
-    expect(true).toBe(true);
+  it("should verify multiplication", async () => {
+    const circuit = await wasm("circuit/mul.circom");
+    const witness = await circuit.calculateWitness({ a: 2, b: 3 });
+
+    await circuit.checkConstraints(witness);
+    await circuit.assertOut(witness, { c: 6 });
   });
 });
-
